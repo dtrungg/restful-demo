e700f98ef00f7b6c05c22756de250b62dcafca23
dtrungg <97575382+dtrungg@users.noreply.github.com>
2025-05-14 16:43:40 +0700

    feat: add log4j2 library

--- src/main/java/org/example/restfuldemo/entity/BaseEntity.java
+++ src/main/java/org/example/restfuldemo/entity/BaseEntity.java
@@ -1,4 +1,17 @@
 package org.example.restfuldemo.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Setter;
+
+//@MappedSuperclass
 public class BaseEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Setter(AccessLevel.NONE)
+//    private Long id;
+
 }
--- src/main/java/org/example/restfuldemo/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/restfuldemo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.restfuldemo.exception;
 
+import lombok.extern.log4j.Log4j2;
 import org.example.restfuldemo.constants.Constants;
 import org.example.restfuldemo.dto.response.ResponseData;
 import org.example.restfuldemo.dto.response.ResponseUtil;
@@ -11,23 +12,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Arrays;
 
+@Log4j2
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ResponseData<?>> handleResourceNotFoundException(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ResponseUtil.error(Arrays.asList(e.getMessage()), Constants.RESOURCE_NOT_FOUND));
     }
 
     @ExceptionHandler(ResponseNotFoundException.class)
     public ResponseEntity<ResponseData<?>> handleResponseNotFoundException(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(ResponseUtil.error(Arrays.asList(e.getMessage()), Constants.RESPONSE_DATA_NOT_FOUND));
     }
 
     @ExceptionHandler(ResourceAlreadyExistException.class)
     public ResponseEntity<ResponseData<?>> handleResourceAlreadyExistException(ResourceAlreadyExistException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(ResponseUtil.error(Arrays.asList(e.getMessage()), Constants.RESOURCE_ALREADY_EXIST));
     }
@@ -37,14 +42,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ResponseUtil.error(Arrays.asList(e.getFieldErrors()
                                 .stream()
-                                .map(error -> error.getDefaultMessage())
-                                .toArray(String[]::new)),
+                                .map(error -> {
+                                    log.error(error.getDefaultMessage());
+                                    return error.getDefaultMessage();
+                                }).toArray(String[]::new)),
                         Constants.RESPONSE_DATA_NOT_FOUND
                 ));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseData<?>> handleGeneralException(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ResponseUtil.error(Arrays.asList(e.getMessage()), Constants.GENERAL_EXCEPTION));
     }
